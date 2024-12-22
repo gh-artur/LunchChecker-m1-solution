6e42c5ffd2223670baf7c481d710cdc5766bdf89
gh-artur <arturfrancisco86@gmail.com>
2024-12-22 13:17:53 -0300

    LunchChecker Module 1 Solution

---
+++ app.js
@@ -0,0 +1,34 @@
+(() =>{
+'use strict';
+
+angular.module('LunchChecker', [])
+
+.controller('LunchCheckerController', LunchCheckerController);
+
+LunchCheckerController.$inject = ['$scope'];
+
+function LunchCheckerController($scope) {
+    $scope.dishes = "";
+    $scope.message = "";
+
+    $scope.checkLunch = function() {
+        var dishesArray = $scope.dishes.split(',');
+
+        console.log(dishesArray);
+
+        if(dishesArray.length == 1 && dishesArray[0] == "") {
+            $scope.message = "Please enter data first";
+            return;
+        }
+
+        if(dishesArray.length <= 3) {
+            $scope.message = "Enjoy!";
+        } else {
+            $scope.message = "Too much!";
+        }
+    }
+
+}
+
+
+})();
